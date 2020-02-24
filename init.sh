32046ec04d80518c80c0011aa024ed11e3240022
Gabriel Volpe <volpegabriel@gmail.com>
2020-02-24 16:48:05 -0300

    git aliases

--- init.sh
+++ init.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-# git config
+# Git config
 git config --global user.email "volpegabriel@gmail.com"
 git config --global user.name "Gabriel Volpe"
+
+# Git aliases
+git config --global alias.amend 'commit --amend -m'
+git config --global alias.br branch
+git config --global alias.cm 'commit -m'
+git config --global alias.co checkout
+git config --global alias.ls 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'
+git config --global alias.ll 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'
+git config --global alias.st status
+
+# SSH key
+ssh-keygen -t rsa -b 4096 -C "volpegabriel@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
