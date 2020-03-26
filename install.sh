95ae8b61cd3b988036598d0492e83fe0f10244a2
Gabriel Volpe <volpegabriel@gmail.com>
2020-03-26 18:24:29 +0100

    fixing paths after trying out a fresh install

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Shows the output of every command
+set +x
+
 # Install Nix
 curl https://nixos.org/nix/install | sh
 
 # Configure Nix
-mkdir -p ~/.config/nixpkgs && cp confix.nix ~/.config/nixpkgs/.
+mkdir -p $HOME/.config/nixpkgs && cp config.nix $HOME/.config/nixpkgs/.
 
 # Install software
 nix-env -irf packages.nix
@@ -14,4 +17,5 @@ cachix use ghcide-nix
 nix-env -iA ghcide-ghc865 -f https://github.com/cachix/ghcide-nix/tarball/master
 
 # Configure software
+chmod +x scripts/config.sh
 scripts/config.sh
--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+set +x
+
 # Git config
-echo ">>> Configuring Git"
 git config --global user.email "volpegabriel@gmail.com"
 git config --global user.name "Gabriel Volpe"
 
@@ -17,48 +18,46 @@ git config --global alias.ll 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset
 git config --global alias.st status
 
 # NeoVim config
-echo ">>> Configuring NeoVim"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir ~/.local/share/nvim/plugged
-cp ../nvim/* ~/.config/nvim/.
+curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p $HOME/.local/share/nvim/plugged
+mkdir -p $HOME/.config/nvim
+cp nvim/* $HOME/.config/nvim/.
 
 # Fixing locales
-echo ">>> Configuring locales"
-echo 'export LC_ALL="en_US.UTF-8"' >> ~/.bashrc
-echo 'export LOCALE_ARCHIVE_2_11="$(nix-build --no-out-link "<nixpkgs>" -A glibcLocales)/lib/locale/locale-archive"' >> ~/.bashrc
-echo 'export LOCALE_ARCHIVE_2_27="$(nix-build --no-out-link "<nixpkgs>" -A glibcLocales)/lib/locale/locale-archive"' >> ~/.bashrc
-echo 'export LOCALE_ARCHIVE="/usr/bin/locale"' >> ~/.bashrc
+echo 'export LC_ALL="en_US.UTF-8"' >> $HOME/.bashrc
+echo 'export LOCALE_ARCHIVE_2_11="$(nix-build --no-out-link "<nixpkgs>" -A glibcLocales)/lib/locale/locale-archive"' >> $HOME/.bashrc
+echo 'export LOCALE_ARCHIVE_2_27="$(nix-build --no-out-link "<nixpkgs>" -A glibcLocales)/lib/locale/locale-archive"' >> $HOME/.bashrc
+echo 'export LOCALE_ARCHIVE="/usr/bin/locale"' >> $HOME/.bashrc
 
 # Spotify in 4k display
 echo ">>> Configuring Spotify in 4k"
-echo 'alias spotify="~/.nix-profile/bin/spotify --force-device-scale-factor=2.0"' >> ~/.bashrc
+echo 'alias spotify="$HOME/.nix-profile/bin/spotify --force-device-scale-factor=2.0"' >> $HOME/.bashrc
 echo ">>> Remember to configure media keys for Spotify: https://fabianlee.org/2016/05/25/ubuntu-enabling-media-keys-for-spotify/"
 
 # Toggle touchpad
-cp toggle-touchpad.sh ~/.
+cp scripts/toggle-touchpad.sh $HOME/.
 echo ">>> Remember to add key-shortcut to Toggle Touchpad"
 
 # Desktop applications with icons
 echo ">>> Configuring desktop apps"
-chmod +x ~/Desktop/*
-sudo cp ../desktop/* /usr/share/applications/.
+sudo cp desktop/* /usr/share/applications/.
 
 # Git parser
-cat git-parser >> ~/.bashrc
+cat scripts/git-parser >> $HOME/.bashrc
 
 # SBT / Sonatype credentials
-echo ">>> Configuring sbt-sonatype"
-cp ../sbt/sonatype_credentials ~/.sbt/.
-cp ../sbt/sonatype.sbt ~/.sbt/1.0/.
+mkdir -p $HOME/.sbt/1.0
+cp sbt/sonatype_credentials $HOME~/.sbt/.
+cp sbt/sonatype.sbt $HOME/.sbt/1.0/.
 echo ">>> Remember to set up username and password for Sonatype"
-
-# Hoogle database
-hoogle generate
 
 # SSH key
 echo ">>> Configuring SSH key"
 ssh-keygen -t rsa -b 4096 -C "volpegabriel@gmail.com"
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+ssh-add $HOME/.ssh/id_rsa
+
+# Hoogle database
+hoogle generate
 
 echo ">>> Installation & Configuration DONE"
