72501301535eb047f9ae8532064862ec33cf90c3
Gabriel Volpe <volpegabriel@gmail.com>
2020-03-28 15:27:32 +0100

    tmux configuration

--- scripts/config.sh
+++ scripts/config.sh
@@ -57,6 +57,12 @@ ssh-keygen -t rsa -b 4096 -C "volpegabriel@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add $HOME/.ssh/id_rsa
 
+# Tmux configuration (including patched font for Ubuntu)
+mkdir -p $HOME/.local/share/fonts/.
+cp tmux/Ubuntu-Mono-Nerd-Font-Complete.ttf $HOME/.local/share/fonts/.
+cp tmux/.tmux.conf $HOME/.
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
 # Hoogle database
 hoogle generate
 
