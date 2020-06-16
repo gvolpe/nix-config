4e66f39be661c9ac86f771965661fe91be2c9d89
gvolpe <volpegabriel@gmail.com>
2020-06-16 17:58:09 +0200

    removing config option

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,10 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  config.allowUnfree = true;
 
   home.packages = with pkgs; [
     cachix # nix caching
