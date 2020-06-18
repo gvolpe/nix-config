1f43a0deeecadf3b5e16e557b277ea8a8b914304
gvolpe <volpegabriel@gmail.com>
2020-06-18 12:36:06 +0200

    pinning nixpkgs

--- install.sh
+++ install.sh
@@ -3,16 +3,19 @@
 # Shows the output of every command
 set +x
 
+# Pin Nixpkgs to NixOS unstable on June 15th of 2020
+export PINNED_NIX_PKGS="https://github.com/NixOS/nixpkgs-channels/archive/0a146054bdf.tar.gz"
+
 # Switch to the unstable channel
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
-sudo nixos-rebuild switch --upgrade
+sudo nixos-rebuild -I nixpkgs=$PINNED_NIX_PKGS switch --upgrade
 
 # Nix configuration
 sudo cp nixos/configuration.nix /etc/nixos/
 cp -r nixos/home/* $HOME/.config/nixpkgs/
 
-# Home manager
-nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+# Home manager - Pin to master on June 15th of 2020
+nix-channel --add https://github.com/rycee/home-manager/archive/ad4f33c.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
 home-manager switch
