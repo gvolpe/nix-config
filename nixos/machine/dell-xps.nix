68e503ac094ccf9868f6bcdb1292e55bde8b8bc3
gvolpe <volpegabriel@gmail.com>
2020-06-22 22:04:42 +0200

    Mounting extra partition for data

--- nixos/machine/dell-xps.nix
+++ nixos/machine/dell-xps.nix
@@ -4,4 +4,9 @@
 {
   boot.loader.grub.device = "/dev/nvme0n1"; # or "nodev" for efi only
   networking.interfaces.wlp2s0.useDHCP = true;
+
+  fileSystems."/data" = { 
+    device = "/dev/nvme0n1p3";
+    fsType = "ext4";
+  };
 }
