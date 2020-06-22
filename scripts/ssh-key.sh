e5d58e5cbea31675bb4b44c7474475d8f75e9c52
gvolpe <volpegabriel@gmail.com>
2020-06-22 21:57:06 +0200

    Migrate script

--- scripts/ssh-key.sh
+++ scripts/ssh-key.sh
@@ -1,9 +1,6 @@
-#! /usr/bin/env bash
-
-# Shows the output of every command
-set +x
+#! /usr/bin/env fish
 
 # Create SSH key
 ssh-keygen -t rsa -b 4096 -C "volpegabriel@gmail.com"
-eval "$(ssh-agent -s)"
+eval (ssh-agent -s)
 ssh-add $HOME/.ssh/id_rsa
