b42a501e0a270f26e1d5d59ee4afc6c7ba4d8a59
gvolpe <volpegabriel@gmail.com>
2020-06-22 22:06:13 +0200

    fix typo

--- nixos/machine/dell-xps.nix
+++ nixos/machine/dell-xps.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 
-# Configuration specic to the machine #
+# Configuration specific to the machine #
 {
   boot.loader.grub.device = "/dev/nvme0n1"; # or "nodev" for efi only
   networking.interfaces.wlp2s0.useDHCP = true;
 
-  fileSystems."/data" = { 
+  fileSystems."/data" = {
     device = "/dev/nvme0n1p3";
     fsType = "ext4";
   };
