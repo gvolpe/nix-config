5a7b54a318ec818472e5ecd578a358f13f519f29
Gabriel Volpe <volpegabriel@gmail.com>
2020-07-11 19:26:25 +0200

    recursive derivation

--- nixos/home/programs/dconf2nix/default.nix
+++ nixos/home/programs/dconf2nix/default.nix
@@ -1,21 +1,19 @@
-{ stdenv, ... }:
+{ stdenv }:
 
-let
+stdenv.mkDerivation rec {
+  name    = "dconf2nix-${version}";
   version = "v0.0.2";
-in
-  stdenv.mkDerivation {
-    name = "dconf2nix-${version}";
 
-    src = builtins.fetchurl {
-      url    = "https://github.com/gvolpe/dconf2nix/releases/download/${version}/dconf2nix-linux-x86-64";
-      sha256 = "135xl48aiqlbbcn2q95aj50p2bzfyk87h4jzbnk81qaak5043krp";
-    };
+  src = builtins.fetchurl {
+    url    = "https://github.com/gvolpe/dconf2nix/releases/download/${version}/dconf2nix-linux-x86-64";
+    sha256 = "135xl48aiqlbbcn2q95aj50p2bzfyk87h4jzbnk81qaak5043krp";
+  };
 
-    phases = ["installPhase" "patchPhase"];
+  phases = ["installPhase" "patchPhase"];
 
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/dconf2nix
-      chmod +x $out/bin/dconf2nix
-    '';
-  }
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/dconf2nix
+    chmod +x $out/bin/dconf2nix
+  '';
+}
