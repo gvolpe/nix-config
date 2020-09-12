10310d7a29190f6ecda0da6f445db720a18ea06a
Gabriel Volpe <volpegabriel@gmail.com>
2020-09-12 23:44:18 +0200

    Update installation script: yet again

--- install.sh
+++ install.sh
@@ -8,16 +8,16 @@ export PINNED_NIX_PKGS="https://github.com/NixOS/nixpkgs-channels/archive/615251
 
 # Switch to the unstable channel
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
-sudo nixos-rebuild -I nixpkgs=$PINNED_NIX_PKGS switch --upgrade
 
 # Nix configuration
 sudo cp system/configuration.nix /etc/nixos/
 sudo mkdir -p /etc/nixos/machine/
 sudo cp system/machine/* /etc/nixos/machine/
-mkdir -p $HOME/.config/nixpkgs/
-cp -r home/* $HOME/.config/nixpkgs/
+sudo nixos-rebuild -I nixpkgs=$PINNED_NIX_PKGS switch --upgrade
 
 # Home manager
+mkdir -p $HOME/.config/nixpkgs/
+cp -r home/* $HOME/.config/nixpkgs/
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
