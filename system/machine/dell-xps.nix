4e96e02e1254780051a99b26c3a823151c752539
Gabriel Volpe <volpegabriel@gmail.com>
2020-12-08 23:08:01 +0100

    configure hostname + xrandr

--- system/machine/dell-xps.nix
+++ system/machine/dell-xps.nix
@@ -13,10 +13,40 @@
     };
   };
 
-  networking.interfaces.wlp2s0.useDHCP = true;
+  networking = {
+    hostName = "dell-xps-15-9560";
+    interfaces.wlp2s0.useDHCP = true;
+  };
 
   fileSystems."/data" = { 
     device = "/dev/nvme0n1p3";
     fsType = "ext4";
+  };
+
+  services.xserver = {
+    videoDrivers = [ "amdgpu" ];
+
+    xrandrHeads = [
+      { output = "HDMI-1";
+        primary = true;
+        monitorConfig = ''
+          Option "PreferredMode" "3840x2160"
+          Option "Position" "0 0"
+        '';
+      }
+      { output = "eDP-1";
+        monitorConfig = ''
+          Option "PreferredMode" "1920x1980"
+          Option "Position" "0 0"
+        '';
+      }
+    ];
+    resolutions = [
+      { x = 2048; y = 1152; }
+      { x = 1920; y = 1080; }
+      { x = 2560; y = 1440; }
+      { x = 3072; y = 1728; }
+      { x = 3840; y = 2160; }
+    ];
   };
 }
