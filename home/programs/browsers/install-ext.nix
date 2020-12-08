36b635a27a9afe27da740e9863ea862c79251f7c
Gabriel Volpe <volpegabriel@gmail.com>
2020-12-08 18:19:49 +0100

    re-install dark-theme as the enabled-dark-mode does not work properly on latest brave

--- home/programs/browsers/install-ext.nix
+++ home/programs/browsers/install-ext.nix
@@ -11,8 +11,9 @@ let
 in
 {
   # see brave.browser.enabled_labs_experiments for more flags
-  xdg.configFile."${baseDir}/Local State".source = ./local-state.json;
+  #xdg.configFile."${baseDir}/Local State".source = ./local-state.json;
 
+  xdg.configFile."${extDir}${x.dark-reader}.json".text        = ext;
   xdg.configFile."${extDir}${x.github-dark-theme}.json".text  = ext;
   xdg.configFile."${extDir}${x.google-meet-grid}.json".text   = ext;
   xdg.configFile."${extDir}${x.google-translate}.json".text   = ext;
