fbe908f6efb71e3e7a6f02b3179a57b32e0fabd9
Gabriel Volpe <volpegabriel@gmail.com>
2020-12-09 14:14:05 +0100

    re-installin github-dark-theme for gists and community

--- home/programs/browsers/install-ext.nix
+++ home/programs/browsers/install-ext.nix
@@ -13,9 +13,10 @@ in
   # see brave.browser.enabled_labs_experiments for more flags
   #xdg.configFile."${baseDir}/Local State".source = ./local-state.json;
 
-  xdg.configFile."${extDir}${x.dark-reader}.json".text      = ext;
-  xdg.configFile."${extDir}${x.google-meet-grid}.json".text = ext;
-  xdg.configFile."${extDir}${x.google-translate}.json".text = ext;
-  xdg.configFile."${extDir}${x.lastpass}.json".text         = ext;
-  xdg.configFile."${extDir}${x.vimium}.json".text           = ext;
+  xdg.configFile."${extDir}${x.dark-reader}.json".text       = ext;
+  xdg.configFile."${extDir}${x.github-dark-theme}.json".text = ext;
+  xdg.configFile."${extDir}${x.google-meet-grid}.json".text  = ext;
+  xdg.configFile."${extDir}${x.google-translate}.json".text  = ext;
+  xdg.configFile."${extDir}${x.lastpass}.json".text          = ext;
+  xdg.configFile."${extDir}${x.vimium}.json".text            = ext;
 }
