08fc986fa0e5059ee45718f0a840df344ff38855
Gabriel Volpe <volpegabriel@gmail.com>
2020-12-12 18:59:23 +0100

    fix status bar

--- home/services/polybar/bar.nix
+++ home/services/polybar/bar.nix
@@ -2,8 +2,33 @@
 
 let
   bar = ''
-    [bar/default]
-    inherit = bar/main
+    [bar/main]
+    monitor = ''${env:MONITOR:eDP}
+    width = 100%
+    height = 48
+    radius = 6.0
+    fixed-center = true
+
+    background = ''${color.bg}
+    foreground = ''${color.fg}
+
+    padding-left = 0
+    padding-right = 0
+
+    module-margin-left = 1
+    module-margin-right = 2
+
+    tray-padding = 3
+    tray-background = ''${color.bg}
+
+    cursor-click = pointer
+    cursor-scroll = ns-resize
+
+    overline-size = 2
+    overline-color = ''${color.ac}
+
+    border-bottom-size = 0
+    border-color = ''${color.ac}
 
     ; Text Fonts
     font-0 = Iosevka Nerd Font:style=Medium:size=${toString font0};3
@@ -15,11 +40,14 @@ let
     font-3 = Iosevka Nerd Font:style=Medium:size=${toString font3};3
     ; Smaller font size for shorter spaces
     font-4 = Iosevka Nerd Font:style=Medium:size=${toString font4};3
+
+    ;font-5 = "Font Awesome 5 Free:style=Solid:pixelsize=18;0"
+    ;font-5 = "MaterialIcons:size=18;0"
   '';
 
   top = ''
     [bar/top]
-    inherit = bar/default
+    inherit = bar/main
 
     tray-position = center
     modules-left = right-end-top xmonad left-end-bottom right-end-top left-end-top
@@ -29,7 +57,7 @@ let
 
   bottom = ''
     [bar/bottom]
-    inherit = bar/default
+    inherit = bar/main
     bottom = true
 
     tray-position = none
