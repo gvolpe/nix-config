5d5e2033b99e648ac29a02f0a66ce2a271814f6e
Gabriel Volpe <volpegabriel@gmail.com>
2020-12-13 16:08:22 +0100

    update neovim-nightly

--- home/programs/neovim/dev/nightly.nix
+++ home/programs/neovim/dev/nightly.nix
@@ -1,14 +1,17 @@
 { pkgs }:
 
-# nightly release from Jul 12
 pkgs.neovim-unwrapped.overrideAttrs (
   old: {
-    version = "v0.5.0-593-g1ca67a73c";
+    name    = "neovim-nightly-2020-12-12";
+    version = "v0.5.0-dev+941-g9c56d8e5f";
+
     src = pkgs.fetchFromGitHub {
       owner  = "neovim";
       repo   = "neovim";
-      rev    = "1ca67a73c0ba680eb8328e68bea31f839855dd29";
-      sha256 = "1568d5xmpnndxfclcjc05i4dp5480fp7qq0jqajz9n0xlrcfsmn2";
+      rev    = "9c56d8e5f792cf770ebe88d9488549b4106b06a9";
+      sha256 = "0yhh9yix6jzcp73skavqhhgnkr0mwx0wkw2nd7cqvvy8xb2r59c7";
     };
+
+    buildInputs = old.buildInputs ++ [ pkgs.tree-sitter ];
   }
 )
