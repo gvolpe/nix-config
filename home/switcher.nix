77d47e3580ec5603510a9091cc9f995122809b6b
Gabriel Volpe <volpegabriel@gmail.com>
2020-12-14 17:28:13 +0100

    generalize switcher for all HDMI ids

--- home/switcher.nix
+++ home/switcher.nix
@@ -6,12 +6,9 @@ in
   pkgs.writeShellScriptBin "hms" ''
     monitors=$(${xrandr} --listmonitors)
 
-    if [[ $monitors == *"HDMI-1"* ]]; then
-      echo "Switching to default HM config for HDMI-1"
+    if [[ $monitors == *"HDMI"* ]]; then
+      echo "Switching to default HM config for HDMI display"
       home-manager -f ${config.xdg.configHome}/nixpkgs/display/hdmi.nix switch
-    elif [[ $monitors == *"HDMI-A-0"* ]]; then
-      echo "Switching to HM config for HDMI-A-0"
-      home-manager -f ${config.xdg.configHome}/nixpkgs/display/edp.nix switch
     elif [[ $monitors == *"eDP"* ]]; then
       echo "Switching to HM config for eDP or eDP-1 (laptop display)"
       home-manager -f ${config.xdg.configHome}/nixpkgs/display/edp.nix switch
