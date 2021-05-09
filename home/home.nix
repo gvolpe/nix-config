75a45c677c813a9a6e1ee01aeacb662841ddb8c7
Gabriel Volpe <volpegabriel@gmail.com>
2021-05-09 17:00:34 +0200

    install discord

--- home/home.nix
+++ home/home.nix
@@ -14,6 +14,7 @@ let
     cachix               # nix caching
     calibre              # e-book reader
     dconf2nix            # dconf (gnome) files to nix converter
+    discord              # chat client for dev stuff
     dmenu                # application launcher
     docker-compose       # docker manager
     dive                 # explore docker layers
