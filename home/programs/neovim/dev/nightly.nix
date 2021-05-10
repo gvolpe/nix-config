7fff0ef45fd2daa247195244d132df704421b2ab
Gabriel Volpe <volpegabriel@gmail.com>
2021-05-10 14:11:34 +0200

    Downgrade neovim version

--- home/programs/neovim/dev/nightly.nix
+++ home/programs/neovim/dev/nightly.nix
@@ -2,14 +2,15 @@
 
 pkgs.neovim-unwrapped.overrideAttrs (
   old: {
-    name    = "neovim-nightly-2021-05-08";
+    # newer versions break coc.nvim (there's a fix in master but idk how to override it)
+    name    = "neovim-nightly-2021-05-07";
     version = "v0.5.0-dev+1309-g516551319";
 
     src = pkgs.fetchFromGitHub {
       owner  = "neovim";
       repo   = "neovim";
-      rev    = "5165513198dbbe7415f1b8c51225ae8f822628ec";
-      sha256 = "0qi9fn2g28g92p6y3gw8mryb41cmxz1sr5m54hl2zb7mq8na4vpg";
+      rev    = "4be0e92db01a502863ac4bb26dd0fee16d833145";
+      sha256 = "1h3sinb52xi4m94dv3dwlzjyr8mzz8v0ndgrkzgi3c1nwmf6b0f2";
     };
 
     buildInputs = old.buildInputs ++ [ pkgs.tree-sitter ];
