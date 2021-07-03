2229650ca544ea56106d8dab7c57c6a90dbcd896
Gabriel Volpe <volpegabriel@gmail.com>
2021-07-03 22:25:25 +0200

    Update neovim to 5.0.0

--- home/programs/neovim/dev/nightly.nix
+++ home/programs/neovim/dev/nightly.nix
@@ -2,15 +2,14 @@
 
 pkgs.neovim-unwrapped.overrideAttrs (
   old: {
-    # newer versions break coc.nvim (there's a fix in master but idk how to override it)
-    name    = "neovim-nightly-2021-05-07";
-    version = "v0.5.0-dev+1309-g516551319";
+    name    = "neovim-5.0.0";
+    version = "v0.5.0";
 
     src = pkgs.fetchFromGitHub {
       owner  = "neovim";
       repo   = "neovim";
-      rev    = "4be0e92db01a502863ac4bb26dd0fee16d833145";
-      sha256 = "1h3sinb52xi4m94dv3dwlzjyr8mzz8v0ndgrkzgi3c1nwmf6b0f2";
+      rev    = "a5ac2f45ff84a688a09479f357a9909d5b914294";
+      sha256 = "0lgbf90sbachdag1zm9pmnlbn35964l3khs27qy4462qzpqyi9fi";
     };
 
     buildInputs = old.buildInputs ++ [ pkgs.tree-sitter ];
