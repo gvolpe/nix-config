6b04abaf691be4137c3d8d8c53d2a1a19cba3f8b
Gabriel Volpe <volpegabriel@gmail.com>
2021-08-20 12:38:29 +0200

    init: CI build

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ touch $HOME/.cache/fzf-hoogle/cache.json
 # Home manager
 mkdir -p $HOME/.config/nixpkgs/
 cp -r home/* $HOME/.config/nixpkgs/
-nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 nix-shell '<home-manager>' -A install
