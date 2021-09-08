2d57ebf46b0dc1822532b933acac187f6cf37d46
Gabriel Volpe <volpegabriel@gmail.com>
2021-09-08 09:03:55 +0200

    simplify color scheme settings for dunst

--- home/services/dunst/default.nix
+++ home/services/dunst/default.nix
@@ -1,22 +1,9 @@
 { config, pkgs, ... }:
 
 let
-  source = fetchTarball {
-    name   = "base16";
-    url    = "https://github.com/atpotts/base16-nix/archive/4f192af.tar.gz";
-    sha256 = "1yf59vpd1i8lb2ml7ha8v6i4mv1b0xwss8ngzw08s39j838gyx6h";
-  };
+  colors = import ../../themes/colors.nix;
 in
 {
-  imports = [ "${source}/base16.nix" ];
-
-  # More schemes at: https://github.com/atpotts/base16-nix/blob/master/schemes.json
-  themes.base16 = {
-    enable  = true;
-    scheme  = "helios";
-    variant = "helios";
-  };
-
   services.dunst = {
     enable = true;
     iconTheme = {
@@ -24,7 +11,7 @@ in
       package = pkgs.beauty-line-icon-theme;
       size = "16x16";
     };
-    settings = with config.lib.base16.theme; {
+    settings = with colors.scheme.helios; {
       global = {
         monitor = 0;
         geometry = "600x50-50+65";
@@ -33,7 +20,7 @@ in
         padding = 16;
         horizontal_padding = 16;
         frame_width = 3;
-        frame_color = "${base00-hex}";
+        frame_color = "${base00}";
         separator_color = "frame";
         font = "JetBrainsMono Nerd Font 10";
         line_height = 4;
@@ -52,19 +39,19 @@ in
         hide_duplicate_count = true;
       };
       urgency_low = {
-        background = "#${base00-hex}";
-        forefround = "#${base05-hex}";
+        background = "#${base00}";
+        forefround = "#${base05}";
         timeout = 4;
       };
       urgency_normal = {
-        background = "#${base00-hex}";
-        forefround = "#${base05-hex}";
+        background = "#${base00}";
+        forefround = "#${base05}";
         timeout = 4;
       };
       urgency_critical = {
-        background = "#${base0A-hex}";
-        forefrond = "#${base0D-hex}";
-        frame_color = "#${base08-hex}";
+        background = "#${base0A}";
+        forefrond = "#${base0D}";
+        frame_color = "#${base08}";
         timeout = 10;
       };
     };
---
+++ home/themes/colors.nix
@@ -0,0 +1,26 @@
+{
+  # More schemes at: https://github.com/atpotts/base16-nix/blob/master/schemes.json
+  scheme = {
+    # https://github.com/reyemxela/base16-helios-scheme
+    helios = {
+      scheme = "Helios";
+      author = "Alex Meyer (https://github.com/reyemxela)";
+      base00 = "1d2021"; # ----
+      base01 = "383c3e"; # ---
+      base02 = "53585b"; # --
+      base03 = "6f7579"; # -
+      base04 = "cdcdcd"; # +
+      base05 = "d5d5d5"; # ++
+      base06 = "dddddd"; # +++
+      base07 = "e5e5e5"; # ++++
+      base08 = "d72638"; # red
+      base09 = "eb8413"; # orange
+      base0A = "f19d1a"; # yellow
+      base0B = "88b92d"; # green
+      base0C = "1ba595"; # aqua/cyan
+      base0D = "1e8bac"; # blue
+      base0E = "be4264"; # purple
+      base0F = "c85e0d"; # brown
+    };
+  };
+}
