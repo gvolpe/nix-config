f19a8a29a9f60e55d2a75b50aa5855a21ff001a2
Gabriel Volpe <volpegabriel@gmail.com>
2021-11-10 21:38:22 +0100

    count lines of code in a git repo

--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -23,6 +23,8 @@ let
       "ssh://git@gitlab.com".pushInsteadOf = "gl:";
     };
   };
+
+  rg = "${pkgs.ripgrep}/bin/rg";
 in
 {
   programs.git = {
@@ -30,6 +32,7 @@ in
     aliases = {
       amend = "commit --amend -m";
       fixup = "!f(){ git reset --soft HEAD~\${1} && git commit --amend -C HEAD; };f";
+      loc   = "!f(){ git ls-files | ${rg} \"\\.\${1}\" | xargs wc -l; };f"; # lines of code
       br = "branch";
       co = "checkout";
       st = "status";
