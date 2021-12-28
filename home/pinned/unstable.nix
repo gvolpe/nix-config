d0031e42d5910f454613d41cd8a0f141ed3580ff
Gabriel Volpe <volpegabriel@gmail.com>
2021-12-28 11:53:22 +0100

    update unstable nixpkgs

--- home/pinned/unstable.nix
+++ home/pinned/unstable.nix
@@ -1,5 +1,5 @@
 builtins.fetchTarball {
-  name   = "nixpkgs-unstable-2021-05-25";
-  url    = "https://github.com/NixOS/nixpkgs/archive/ea7d4aa9b822.tar.gz";
-  sha256 = "11bwgyglag40w2y50nq2gg4697ymdjprkgip75kb3hjrvvwpma53";
+  name   = "nixpkgs-unstable-2021-12-27";
+  url    = "https://github.com/NixOS/nixpkgs/archive/ac169ec6371f.tar.gz";
+  sha256 = "0bwjyz15sr5f7z0niwls9127hikp2b6fggisysk0cnk3l6fa8abh";
 }
