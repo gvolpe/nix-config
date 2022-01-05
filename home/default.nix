1bddf3c00074314c6ab384d10a63617dbfbd21bc
Gabriel Volpe <volpegabriel@gmail.com>
2022-01-05 18:26:24 +0100

    update home-manager version name and hash

--- home/default.nix
+++ home/default.nix
@@ -4,9 +4,9 @@ let
   hm_url = pkgs.lib.fileContents ../pinned/home-manager;
 
   home-manager = builtins.fetchTarball {
-    name   = "home-manager-2021-08-21";
+    name   = "home-manager-2022-01-05";
     url    = hm_url;
-    sha256 = "0s8nlgrf16bz2bhnk0xrzvivagq55cifxf2p545c7n4zj9ryfkkp";
+    sha256 = "0nbvvxyix07679sz6713civqqfvnxrnrcrbjjkqn96p3yna5a3mf";
   };
 
   evalHome = import "${toString home-manager}/modules";
