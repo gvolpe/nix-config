fd044a3435d6193ee49d8a078aa79966eb20f90e
Gabriel Volpe <volpegabriel@gmail.com>
2022-07-31 12:07:17 +0200

    refactor firefox module

--- home/modules/firefoxie.nix
+++ home/modules/firefoxie.nix
@@ -4,81 +4,6 @@ with lib;
 
 let
   cfg = config.programs.firefoxie;
-
-  # disable the annoying floating icon with camera and mic when on a call
-  disableWebRtcIndicator = ''
-    #webrtcIndicator {
-      display: none;
-    }
-  '';
-
-  # DPI settings
-  dpiSettings = {
-    "layout.css.devPixelsPerPx" = if cfg.hidpi then "-1.0" else "1.25";
-  };
-
-  # ~/.mozilla/firefox/PROFILE_NAME/prefs.js | user.js
-  sharedSettings = {
-    "app.normandy.first_run" = false;
-    "app.shield.optoutstudies.enabled" = false;
-
-    # disable updates (pretty pointless with nix)
-    "app.update.channel" = "default";
-
-    "browser.contentblocking.category" = "standard"; # "strict"
-    "browser.ctrlTab.recentlyUsedOrder" = false;
-
-    "browser.download.useDownloadDir" = false;
-    "browser.download.viewableInternally.typeWasRegistered.svg" = true;
-    "browser.download.viewableInternally.typeWasRegistered.webp" = true;
-    "browser.download.viewableInternally.typeWasRegistered.xml" = true;
-
-    "browser.link.open_newwindow" = true;
-
-    "browser.search.region" = "PL";
-    "browser.search.widget.inNavBar" = true;
-
-    "browser.shell.checkDefaultBrowser" = false;
-    "browser.startup.homepage" = "https://nixos.org";
-    "browser.tabs.loadInBackground" = true;
-    "browser.urlbar.placeholderName" = "DuckDuckGo";
-    "browser.urlbar.showSearchSuggestionsFirst" = false;
-
-    "distribution.searchplugins.defaultLocale" = "en-US";
-
-    "doh-rollout.balrog-migration-done" = true;
-    "doh-rollout.doneFirstRun" = true;
-
-    "dom.forms.autocomplete.formautofill" = false;
-
-    "general.autoScroll" = true;
-    "general.useragent.locale" = "en-US";
-
-    "extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
-
-    "extensions.extensions.activeThemeID" = "firefox-alpenglow@mozilla.org";
-    "extensions.update.enabled" = false;
-    "extensions.webcompat.enable_picture_in_picture_overrides" = true;
-    "extensions.webcompat.enable_shims" = true;
-    "extensions.webcompat.perform_injections" = true;
-    "extensions.webcompat.perform_ua_overrides" = true;
-
-    "print.print_footerleft" = "";
-    "print.print_footerright" = "";
-    "print.print_headerleft" = "";
-    "print.print_headerright" = "";
-
-    "privacy.donottrackheader.enabled" = true;
-
-    # Yubikey
-    "security.webauth.u2f" = true;
-    "security.webauth.webauthn" = true;
-    "security.webauth.webauthn_enable_softtoken" = true;
-    "security.webauth.webauthn_enable_usbtoken" = true;
-
-    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-  } // dpiSettings;
-
 in
 {
   meta.maintainers = [ hm.maintainers.gvolpe ];
@@ -99,45 +24,9 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    programs.firefox = {
-      enable = true;
-
-      extensions = with cfg.addons; [
-        bitwarden
-        darkreader
-        # auto-accepts cookies, use only with privacy-badger & ublock-origin
-        i-dont-care-about-cookies
-        languagetool
-        link-cleaner
-        privacy-badger
-        to-deepl
-        ublock-origin
-        unpaywall
-        vimium
-      ];
-
-      package = pkgs.firefox-beta-bin;
-
-      profiles = {
-        default = {
-          id = 0;
-          settings = sharedSettings;
-          userChrome = disableWebRtcIndicator;
-        };
-
-        chatroulette = {
-          id = 1;
-          settings = sharedSettings;
-          userChrome = disableWebRtcIndicator;
-        };
-
-        demo = {
-          id = 2;
-          settings = sharedSettings;
-          userChrome = disableWebRtcIndicator;
-        };
-      };
-    };
-  };
+  config = mkIf cfg.enable (
+    import ../programs/browsers/firefox.nix {
+      inherit (cfg) addons hidpi; inherit pkgs;
+    }
+  );
 }
