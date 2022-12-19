ca2c4f4371208fe0de3ea3a23fd84418a37ab999
Gabriel Volpe <volpegabriel@gmail.com>
2022-12-19 18:43:26 +0100

    gnome-keyring: enable user service

--- home/services/default.nix
+++ home/services/default.nix
@@ -2,6 +2,11 @@ let
   more = {
     services = {
       flameshot.enable = true;
+
+      gnome-keyring = {
+        enable = true;
+        components = [ "pkcs11" "secrets" "ssh" ];
+      };
     };
   };
 in
