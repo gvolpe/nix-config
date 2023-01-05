2575a96a1c35a07d67bc126666b037a31e7a0d54
gvolpe <gvolpe@users.noreply.github.com>
2023-01-05 08:07:37 +0000

    Update metals to latest version

--- home/programs/neovim-ide/metals.nix
+++ home/programs/neovim-ide/metals.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
 pkgs.metalsBuilder {
-  version = "0.11.10+11-8dc6b6fd-SNAPSHOT";
-  outputHash = "sha256-i1fdKuXtYe5wRkww0XsJ/DjA6/iV5qYYOrXG9LZieng=";
+  version = "0.11.10+14-1ea955ec-SNAPSHOT";
+  outputHash = "sha256-uOl/qyaqlhYHZnC21AZsmq8quwq+u8OslUihi0WQ4xs=";
 }
