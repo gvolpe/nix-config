94ea3af7af45eb7518a5a9b228cbedadb0952622
Gabriel Volpe <volpegabriel@gmail.com>
2023-01-30 10:50:58 +0100

    install ouch command-line tool

--- home/home.nix
+++ home/home.nix
@@ -40,6 +40,7 @@ let
     ngrok                # secure tunneling to localhost
     nix-index            # locate packages containing certain nixpkgs
     nyancat              # the famous rainbow cat!
+    ouch                 # painless compression and decompression for your terminal
     pavucontrol          # pulseaudio volume control
     paprefs              # pulseaudio preferences
     pasystray            # pulseaudio systray
