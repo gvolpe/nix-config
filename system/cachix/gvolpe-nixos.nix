38a83f55d8c29835673306d724355bc316606b98
Gabriel Volpe <volpegabriel@gmail.com>
2023-02-08 09:32:58 +0100

    add missing garnix cache substituter

--- system/cachix/gvolpe-nixos.nix
+++ system/cachix/gvolpe-nixos.nix
@@ -3,6 +3,7 @@
     substituters = [
       "https://cache.cachix.io"
       "https://gvolpe-nixos.cachix.org"
+      "https://cache.garnix.io"
     ];
     trusted-public-keys = [
       "gvolpe-nixos.cachix.org-1:0MPlBIMwYmrNqoEaYTox15Ds2t1+3R+6Ycj0hZWMcL0="
