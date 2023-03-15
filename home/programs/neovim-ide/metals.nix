472cd25710f18bfe6b971195c1996b569a3b7c9b
gvolpe <gvolpe@users.noreply.github.com>
2023-03-15 08:07:34 +0000

    Update Metals to 0.11.11+13-a368d7ca-SNAPSHOT

--- home/programs/neovim-ide/metals.nix
+++ home/programs/neovim-ide/metals.nix
@@ -1,6 +1,6 @@
 { metalsBuilder }:
 
 metalsBuilder {
-  version = "0.11.11+9-000a0137-SNAPSHOT";
-  outputHash = "sha256-fL28khIwJlK8fgedLAGofWUoTtaw4mfe2eQraNN7Cyk=";
+  version = "0.11.11+13-a368d7ca-SNAPSHOT";
+  outputHash = "sha256-5LzKSNg4AyjIbP85eCedxjMoJuM1bCnyhTEEdy9kg08=";
 }
