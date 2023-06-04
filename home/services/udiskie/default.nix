ba4b64254c21167a2f44870ac9d2602a2d5a2135
Gabriel Volpe <volpegabriel@gmail.com>
2023-06-04 11:24:25 +0200

    udiskie: set mimeo as file manager

--- home/services/udiskie/default.nix
+++ home/services/udiskie/default.nix
@@ -1,6 +1,15 @@
+{ pkgs, ... }:
+
 {
   services.udiskie = {
     enable = true;
+    automount = true;
+    notify = true;
+    settings = {
+      program_options = {
+        file_manager = "${pkgs.mimeo}/bin/mimeo";
+      };
+    };
     tray = "always";
   };
 }
