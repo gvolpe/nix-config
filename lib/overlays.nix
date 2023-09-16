1eb5cad414198c4652b668d15f263875b41b29d6
Gabriel Volpe <volpegabriel@gmail.com>
2023-09-16 14:04:15 +0200

    instantiate pkgs only once

---
+++ lib/overlays.nix
@@ -0,0 +1,42 @@
+{ inputs, system }:
+
+with inputs;
+
+let
+  cowsayOverlay = f: p: {
+    inherit (inputs.cowsay.packages.${system}) cowsay;
+  };
+
+  fishOverlay = f: p: {
+    inherit fish-bobthefish-theme fish-keytool-completions;
+  };
+
+  nautilusOverlay = f: p: {
+    nautilus-gtk3 = nixpkgs-nautilus-gtk3.legacyPackages.${system}.gnome.nautilus;
+  };
+
+  libOverlay = f: p: rec {
+    libx = import ./. { inherit (p) lib; };
+    lib = p.lib.extend (_: _: {
+      inherit (libx) removeNewline secretManager;
+    });
+  };
+
+  sxmOverlay =
+    if (builtins.hasAttr "sxm-flake" inputs)
+    then sxm-flake.overlays.default
+    else (f: p: { });
+in
+[
+  cowsayOverlay
+  fishOverlay
+  libOverlay
+  nautilusOverlay
+  nurpkgs.overlay
+  neovim-flake.overlays.${system}.default
+  statix.overlays.default
+  sxmOverlay
+  (import ../home/overlays/bat-lvl)
+  (import ../home/overlays/nautilus)
+  (import ../home/overlays/ranger)
+]
--- outputs/nixos-conf.nix
+++ outputs/nixos-conf.nix
@@ -1,29 +1,7 @@
-{ inputs, system, ... }:
+{ inputs, system, pkgs, lib ? pkgs.lib, ... }:
 
 let
   inherit (inputs.nixpkgs.lib) nixosSystem;
-
-  libx = import ../lib { inherit (inputs.nixpkgs) lib; };
-
-  lib = inputs.nixpkgs.lib.extend (_: _: {
-    inherit (libx) secretManager;
-  });
-
-  sxmOverlay =
-    if (builtins.hasAttr "sxm-flake" inputs)
-    then inputs.sxm-flake.overlays.default
-    else (f: p: { });
-
-  pkgs = import inputs.nixpkgs {
-    inherit system;
-    config = {
-      allowUnfree = true;
-      permittedInsecurePackages = [
-        "xrdp-0.9.9"
-      ];
-    };
-    overlays = [ sxmOverlay ];
-  };
 in
 {
   dell-xps = nixosSystem {
