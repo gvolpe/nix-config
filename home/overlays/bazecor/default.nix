98c8a3fce00b15d5c0e50fc887d884a62881c862
Gabriel Volpe <volpegabriel@gmail.com>
2023-11-18 13:27:34 +0100

    bazecor: simplify overlay using the upstream package

--- home/overlays/bazecor/default.nix
+++ home/overlays/bazecor/default.nix
@@ -1,57 +1,11 @@
 self: super:
 
-let
-  # Waiting for PR: https://github.com/NixOS/nixpkgs/pull/254264
-  buildBazecor =
-    { lib
-    , appimageTools
-    , fetchurl
-    }:
-    appimageTools.wrapAppImage rec {
-      pname = "bazecor";
-      version = "1.3.6";
-
-      src = appimageTools.extract {
-        inherit pname version;
-
-        src = fetchurl {
-          url = "https://github.com/Dygmalab/Bazecor/releases/download/v.${version}/Bazecor-${version}-x64.AppImage";
-          hash = "sha256-Mz7T/AAlyfMzdfy/ZV4AEP3ClTolwr2pPzkSCPL66/w=";
-        };
-
-        postExtract = ''
-          # Disable udev rules check to fix this annoying issue: https://github.com/Dygmalab/Bazecor/issues/370
-          substituteInPlace $out/usr/lib/bazecor/resources/app/.webpack/main/index.js \
-            --replace "checkUdev=()=>{try{if(c.default.existsSync(f))return c.default.readFileSync(f,\"utf-8\").trim()===l.trim()}catch(e){console.error(e)}return!1}" "checkUdev=()=>{return 1}"
-        '';
-      };
-
-      extraPkgs = p: (appimageTools.defaultFhsEnvArgs.multiPkgs p) ++ [
-        p.glib
-      ];
-
-      # Also expose the udev rules here, so it can be used as:
-      #   services.udev.packages = [ pkgs.bazecor ];
-      # to allow non-root modifications to the keyboards.
-      extraInstallCommands = ''
-        mv $out/bin/bazecor-${version} $out/bin/bazecor
-
-        mkdir -p $out/lib/udev/rules.d
-        ln -s --target-directory=$out/lib/udev/rules.d ${./60-dygma.rules}
-      '';
-
-      meta = {
-        description = "Graphical configurator for Dygma Products";
-        homepage = "https://github.com/Dygmalab/Bazecor";
-        changelog = "https://github.com/Dygmalab/Bazecor/releases/tag/v${version}";
-        sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
-        license = lib.licenses.gpl3Only;
-        maintainers = with lib.maintainers; [ gvolpe ];
-        platforms = [ "x86_64-linux" ];
-        mainProgram = "bazecor";
-      };
-    };
-in
 {
-  bazecor = self.callPackage buildBazecor { };
+  bazecor = super.applyPatches {
+    src = super.bazecor;
+    postPatch = ''
+      rm lib/udev/rules.d/*-10-dygma.rules
+      ln -s --target-directory=lib/udev/rules.d ${./60-dygma.rules}
+    '';
+  };
 }
