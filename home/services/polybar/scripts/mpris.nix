6a5024699057843a13b80529aa987b5e935e2468
Gabriel Volpe <volpegabriel@gmail.com>
2024-01-21 17:42:49 +0100

    polybar: add scrolling text to spotify metadata

--- home/services/polybar/scripts/mpris.nix
+++ home/services/polybar/scripts/mpris.nix
@@ -1,8 +1,24 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 let
   pctl = "${pkgs.playerctl}/bin/playerctl";
+  zsc = "${pkgs.zscroll}/bin/zscroll";
+
+  format = ''
+    {{ artist }} - {{ title }} (from ‟{{ album }}”) - Duration: {{ duration(mpris:length) }}
+  '';
+
+  status = pkgs.writeShellScriptBin "spotify_status" ''
+    ${pctl} --player=spotify,%any metadata --format '${format}'
+  '';
 in
-  pkgs.writeShellScriptBin "mpris" ''
-    echo $(${pctl} --player=spotify,%any metadata --format '{{ artist }} - {{ title }}')
-  ''
+# Credits to https://github.com/PrayagS/polybar-spotify for the scrolling idea
+pkgs.writeShellScriptBin "mpris" ''
+  ${zsc} -l 40 \
+         --delay 0.3 \
+         --scroll-padding "  " \
+         --match-command "${status}/bin/spotify_status" \
+         --update-check true "${status}/bin/spotify_status" &
+
+  wait
+''
