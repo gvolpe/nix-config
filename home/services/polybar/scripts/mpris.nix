150d9fc5f53facc7e23df763291570232a892e11
Gabriel Volpe <volpegabriel@gmail.com>
2024-01-21 17:51:44 +0100

    polybar: differentiate metadata from status for pausing/resume behavior

--- home/services/polybar/scripts/mpris.nix
+++ home/services/polybar/scripts/mpris.nix
@@ -8,8 +8,12 @@ let
     {{ artist }} - {{ title }} (from ‟{{ album }}”) - Duration: {{ duration(mpris:length) }}
   '';
 
-  status = pkgs.writeShellScriptBin "spotify_status" ''
+  metadata = pkgs.writeShellScriptBin "spotify_metadata" ''
     ${pctl} --player=spotify,%any metadata --format '${format}'
+  '';
+
+  status = pkgs.writeShellScriptBin "spotify_status" ''
+    ${pctl} --player=spotify status
   '';
 in
 # Credits to https://github.com/PrayagS/polybar-spotify for the scrolling idea
@@ -18,7 +22,9 @@ pkgs.writeShellScriptBin "mpris" ''
          --delay 0.3 \
          --scroll-padding "  " \
          --match-command "${status}/bin/spotify_status" \
-         --update-check true "${status}/bin/spotify_status" &
+         --match-text "Playing" "--scroll 1" \
+         --match-text "Paused" "--scroll 0" \
+         --update-check true "${metadata}/bin/spotify_metadata" &
 
   wait
 ''
