7c7e060dc72d0509791dc0d20957c55cdacfd038
Gabriel Volpe <volpegabriel@gmail.com>
2024-02-10 17:32:53 +0100

    nautilus: a few more dconf settings that do not seem to work either...

--- home/programs/dconf/default.nix
+++ home/programs/dconf/default.nix
@@ -16,8 +16,35 @@ with lib.hm.gvariant;
     "org/gnome/nautilus/preferences" = {
       default-folder-viewer = "icon-view";
       default-sort-order = "type";
+      migrated-gtk-settings = true;
       search-filter-time-type = "last_modified";
       search-view = "list-view";
+    };
+
+    "org/gnome/nautilus/window-state" = {
+      initial-size = mkTuple [ 500 400 ];
+      maximized = false;
+      sidebar-width = 200;
+      start-with-sidebar = true;
+    };
+
+    "org/gtk/gtk4/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = false;
+      show-size-column = true;
+      show-type-column = true;
+      sidebar-width = 263;
+      sort-column = "name";
+      sort-directories-first = true;
+      sort-order = "ascending";
+      type-format = "category";
+      window-size = mkTuple [ 100 100 ];
+    };
+
+    "org/gtk/settings/file-chooser" = {
+      window-position = mkTuple [ (-1) (-1) ];
+      window-size = mkTuple [ 300 100 ];
     };
   };
 }
