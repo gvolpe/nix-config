b1b91f15e3981c2628c41ce230cb1b79afc43ed0
Gabriel Volpe <volpegabriel@gmail.com>
2024-02-28 10:19:57 +0100

    khal: fix build

--- home/programs/khal/default.nix
+++ home/programs/khal/default.nix
@@ -1,46 +1,47 @@
 { ... }:
 
 {
-  accounts.calendar.accounts.gvolpe = {
-    primary = true;
-    khal = {
-      enable = true;
-      color = "light green";
-      glob = "*";
-      priority = 10;
-      readOnly = false;
-      type = "discover";
+  accounts.calendar = {
+    basePath = "kalendar";
+    accounts.gvolpe = {
+      primary = true;
+      khal = {
+        enable = true;
+        color = "light green";
+        glob = "*";
+        priority = 10;
+        readOnly = false;
+        type = "discover";
+      };
+      # see https://github.com/nix-community/home-manager/issues/4675#issuecomment-1967853432
+      local = { }; # workaround 1
+      primaryCollection = "gvolpe"; # workaround 2
     };
   };
 
   programs.khal = {
     enable = true;
-    locale = { };
-    # Format strings are for Python strftime, similarly to strftime(3).
-    #dateformat = "%x";
-    #datetimeformat = "%c";
-    ##default.timezone = "UTC+1";
-    ## Monday is 0, Sunday is 6
-    #firstweekday = 0;
-    #longdateformat = "%x";
-    #longdatetimeformat = "%c";
-    #timeformat = "%X";
-    #unicode_symbols = true;
-    #};
-
-    #settings = {
-    ##gvolpe = {
-    ##default_calendar = "Calendar";
-    ##timedelta = "5d";
-    ##};
-    #default = {
-    #default_calendar = "Calendar";
-    #timedelta = "5d";
-    #};
-    #view = {
-    #agenda_event_format =
-    #"{calendar-color}{cancelled}{start-end-time-style} {title}{repeat-symbol}{reset}";
-    #};
-    #};
+    locale = {
+      # Format strings are for Python strftime, similarly to strftime(3).
+      dateformat = "%x";
+      datetimeformat = "%c";
+      #default_timezone = "UTC+1";
+      # Monday is 0, Sunday is 6
+      firstweekday = 0;
+      longdateformat = "%x";
+      longdatetimeformat = "%c";
+      timeformat = "%X";
+      unicode_symbols = true;
+    };
+    settings = {
+      default = {
+        default_calendar = "gvolpe";
+        timedelta = "5d";
+      };
+      view = {
+        agenda_event_format =
+          "{calendar-color}{cancelled}{start-end-time-style} {title}{repeat-symbol}{reset}";
+      };
+    };
   };
 }
