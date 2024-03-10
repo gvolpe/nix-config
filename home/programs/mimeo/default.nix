66f0110029492e6f34b9123d0b1221b529af7fbe
Gabriel Volpe <volpegabriel@gmail.com>
2024-03-10 13:36:34 +0100

    fooooo

--- home/programs/mimeo/default.nix
+++ home/programs/mimeo/default.nix
@@ -2,8 +2,7 @@
 
 let
   # xdg-utils as a runtime dependency: https://github.com/NixOS/nixpkgs/pull/181171
-  # postInstall broken after these changes: https://github.com/NixOS/nixpkgs/pull/285233
-  # use postFixup instead
+  # see on postFixup instead of postInstall: https://github.com/NixOS/nixpkgs/pull/285233
   xdg-mimeo = pkgs.xdg-utils.overrideAttrs (old: {
     postFixup = ''
       cp ${pkgs.mimeo}/bin/mimeo $out/bin/xdg-open
