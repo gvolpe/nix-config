29533b10cce0945d612a3fb0e994843209175d40
Gabriel Volpe <volpegabriel@gmail.com>
2024-03-10 12:25:59 +0100

    metals: update to 1.2.2

--- home/programs/neovim-ide/metals.nix
+++ home/programs/neovim-ide/metals.nix
@@ -1,6 +1,6 @@
 { metalsBuilder }:
 
 metalsBuilder {
-  version = "1.2.0";
-  outputHash = "sha256-nikQ/GFRWmYYzboc9TWIi9gd5kwgCxOLhvIEQWusFik=";
+  version = "1.2.2";
+  outputHash = "sha256-xk2ionn/lBV8AR7n7OR03UuRCoP1/K6KuohhpRwFock=";
 }
