176ae62a5e8ccc70b69d3198e0817fd30ac4776f
Gabriel Volpe <volpegabriel@gmail.com>
2024-07-13 19:48:44 +0200

    hyfetch: add json config

--- home/programs/neofetch/default.nix
+++ home/programs/neofetch/default.nix
@@ -3,5 +3,6 @@
 # command-line system information
 {
   home.packages = with pkgs; [ hyfetch neofetch ];
+  xdg.configFile."hyfetch.json".source = ./hyfetch.json;
   xdg.configFile."neofetch/config.conf".source = ./electric.conf;
 }
