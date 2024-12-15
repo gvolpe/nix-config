0a8737c2ccf3cdf6a040def82e66b6402eafc1ac
Gabriel Volpe <volpegabriel@gmail.com>
2024-12-15 12:40:18 +0100

    incremental build for xdg override

--- home/programs/mimeo/default.nix
+++ home/programs/mimeo/default.nix
@@ -1,13 +1,20 @@
 { pkgs, ... }:
 
 let
+  # https://nixos.org/manual/nixpkgs/unstable/#sec-checkpoint-build
+  inherit (pkgs.checkpointBuildTools) prepareCheckpointBuild mkCheckpointBuild;
+
+  xdgCheckpoint = prepareCheckpointBuild pkgs.xdg-utils;
+
   # xdg-utils as a runtime dependency: https://github.com/NixOS/nixpkgs/pull/181171
   # see on postFixup instead of postInstall: https://github.com/NixOS/nixpkgs/pull/285233
-  xdg-mimeo = pkgs.xdg-utils.overrideAttrs (old: {
+  xdgAsMimeo = pkgs.xdg-utils.overrideAttrs (old: {
     postFixup = ''
       cp ${pkgs.mimeo}/bin/mimeo $out/bin/xdg-open
     '';
   });
+
+  xdg-mimeo = mkCheckpointBuild xdgAsMimeo xdgCheckpoint;
 in
 {
   home.packages = [ pkgs.mimeo xdg-mimeo ];
