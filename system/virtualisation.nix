4888aac63ff1096ac1b000f12714f10d0d886e87
Gabriel Volpe <volpegabriel@gmail.com>
2024-12-25 18:38:05 +0100

    enable virtual manager and quickemu

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -1,9 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 # docs: https://nixos.wiki/wiki/NixOS:nixos-rebuild_build-vm
 {
+  programs.virt-manager.enable = true;
+
+  environment.systemPackages = with pkgs; [ quickemu ];
+
   users = {
     groups.nixosvmtest = { };
+    groups.libvirtd.members = [ "gvolpe" ];
 
     users.vmtest = {
       isSystemUser = true;
@@ -12,7 +17,13 @@
     };
   };
 
-  virtualisation.vmVariant = {
+  virtualisation = {
+    # virtual manager for vms
+    libvirtd.enable = true;
+    spiceUSBRedirection.enable = true;
+  };
+
+  vmVariant = {
     virtualisation = {
       memorySize = 4096;
       cores = 4;
