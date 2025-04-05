88f77098251773f43a5b13feec873a13c2df90cb
Gabriel Volpe <volpegabriel@gmail.com>
2025-04-05 10:44:12 +0200

    hyprland: use v0.46.2

--- home/overlays/hyprland/default.nix
+++ home/overlays/hyprland/default.nix
@@ -2,18 +2,18 @@ self: super:
 
 let
   # see: https://github.com/hyprwm/Hyprland/issues/9518
-  commit = "74d0f34cf396f4fc41ee98b000e7d0a4e277c066";
+  version = "0.46.2";
 in
 {
   hyprland = super.hyprland.overrideAttrs (old: {
-    version = "main-${commit}";
+    inherit version;
 
     src = super.fetchFromGitHub {
       owner = "hyprwm";
       repo = "hyprland";
       fetchSubmodules = true;
-      rev = "${commit}";
-      hash = "sha256-8EM3SWF+GRv6CKW99UFQvC+/6586Upb5QSN2jb+Bjoo=";
+      rev = "v${version}";
+      hash = "sha256-dj9dpVwpyTmUyVu4jtaIU39bHgVkoZjv6cgYfWyHc9E=";
     };
   });
 }
