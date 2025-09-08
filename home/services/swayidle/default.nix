6b3564b04222e83f5282650918668576c590d5f2
Gabriel Volpe <volpegabriel@gmail.com>
2025-09-08 17:55:23 +0200

    update swaylock settings

--- home/services/swayidle/default.nix
+++ home/services/swayidle/default.nix
@@ -10,12 +10,12 @@ let
     	--indicator-thickness 7 \
     	--effect-blur 7x5 \
     	--effect-vignette 0.5:0.5 \
-    	--ring-color bb00cc \
-    	--key-hl-color 880033 \
+    	--ring-color 15236E \
+    	--key-hl-color 348FEB \
     	--line-color 00000000 \
     	--inside-color 00000088 \
     	--separator-color 00000000 \
-    	--grace 2 \
+    	--grace 3 \
     	--fade-in 0.2
   '';
 in
