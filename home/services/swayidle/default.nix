72fd887acc043170880b58573f3c2e1e3266610c
Gabriel Volpe <volpegabriel@gmail.com>
2025-09-08 13:45:53 +0200

    swaylock effects

--- home/services/swayidle/default.nix
+++ home/services/swayidle/default.nix
@@ -1,19 +1,38 @@
 { lib, pkgs, ... }:
 
+let
+  lockcmd-bin = pkgs.writeShellScriptBin "swaylock-cmd" ''
+    ${pkgs.swaylock-effects}/bin/swaylock \
+    	--screenshots \
+    	--clock \
+    	--indicator \
+    	--indicator-radius 100 \
+    	--indicator-thickness 7 \
+    	--effect-blur 7x5 \
+    	--effect-vignette 0.5:0.5 \
+    	--ring-color bb00cc \
+    	--key-hl-color 880033 \
+    	--line-color 00000000 \
+    	--inside-color 00000088 \
+    	--separator-color 00000000 \
+    	--grace 2 \
+    	--fade-in 0.2
+  '';
+in
 {
   # Super+Alt+L in the default setting (screen locker)
-  programs.swaylock.enable = true;
+  home.packages = [ lockcmd-bin ];
 
   services.swayidle = {
     enable = true;
 
     events = [
-      { event = "before-sleep"; command = "${lib.exe pkgs.swaylock} -fF"; }
+      { event = "before-sleep"; command = lib.exe lockcmd-bin; }
       { event = "lock"; command = "lock"; }
     ];
 
     timeouts = [
-      { timeout = 90; command = "${lib.exe pkgs.swaylock} -fF"; }
+      { timeout = 90; command = lib.exe lockcmd-bin; }
     ];
   };
 }
