e8963b55ab79b5a40c449124e36d1cdfe8027706
Gabriel Volpe <volpegabriel@gmail.com>
2025-09-13 16:20:52 +0200

    fuzzel: update background color

--- home/programs/fuzzel/default.nix
+++ home/programs/fuzzel/default.nix
@@ -11,7 +11,7 @@
         terminal = lib.exe pkgs.kitty;
       };
       colors = {
-        background = "0e0730aa";
+        background = "271d45ff";
         prompt = "c8b2d7fa";
         text =   "ebf9fcaa";
       };
