2d65e1fd77a8bc9297a9eeab498cc59d7778e14d
Gabriel Volpe <volpegabriel@gmail.com>
2025-10-02 22:43:58 +0200

    sway-audio-idle-inhibit: filter out pavucontrol

--- home/overlays/sway-audio-idle-inhibit/default.nix
+++ home/overlays/sway-audio-idle-inhibit/default.nix
@@ -9,6 +9,13 @@ in
 {
   sway-audio-idle-inhibit = super.sway-audio-idle-inhibit.overrideAttrs (old: {
     preConfigure = (old.preConfigure or "") + ''
+      # https://github.com/ErikReider/SwayAudioIdleInhibit/issues/36
+      substituteInPlace src/main.cpp \
+        --replace-fail 'strtok_r(argv[++i], " ", &saveptr);' 'strtok_r(argv[++i], ",", &saveptr);'
+      substituteInPlace src/main.cpp \
+        --replace-fail 'token = strtok_r(nullptr, " ", &saveptr);' 'token = strtok_r(nullptr, ",", &saveptr);'
+
+      # https://github.com/ErikReider/SwayAudioIdleInhibit/pull/27
       substituteInPlace src/pulse.cpp \
         --replace-fail "if (appName) {" "${newcode}"
     '';
