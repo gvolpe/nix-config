ff3aaf9155e912ef4f8ce029255cd1f85f1ad65e
Gabriel Volpe <volpegabriel@gmail.com>
2026-04-14 18:48:37 +0200

    Update Metals to 1.6.7 (#327)
    Co-authored-by: gvolpe <gvolpe@users.noreply.github.com>

--- home/programs/neovim-ide/metals.nix
+++ home/programs/neovim-ide/metals.nix
@@ -1,6 +1,6 @@
 { metalsBuilder }:
 
 metalsBuilder {
-  version = "1.6.6";
-  outputHash = "sha256-Snx4JvWOTkJcihVRwj25op4BJqmChz+1fZH/PrCCbt0=";
+  version = "1.6.7";
+  outputHash = "sha256-bGx3PQGgaTueQ/v/Xk7gp03TzllyMs7nCx9QWXNFdt0=";
 }
