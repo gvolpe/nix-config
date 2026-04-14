d5898638e951ee7c5559dee1d00bf20a0d9fff07
Gabriel Volpe <volpegabriel@gmail.com>
2026-04-14 19:32:35 +0200

    update neovim-flake

--- home/themes/default.nix
+++ home/themes/default.nix
@@ -1,25 +1,30 @@
 [
-  ({ pkgs, ... }: {
-    gtk = rec {
-      enable = true;
-      iconTheme = {
-        name = "BeautyLine";
-        package = pkgs.beauty-line-icon-theme;
-      };
-      theme = {
-        name = "Juno-ocean";
-        package = pkgs.juno-theme;
-      };
-      gtk4 = {
-        extraConfig = {
-          gtk-application-prefer-dark-theme = true;
+  ({ pkgs, config, ... }:
+    let
+      cfg = config.gtk;
+    in
+    {
+      gtk = {
+        enable = true;
+        iconTheme = {
+          name = "BeautyLine";
+          package = pkgs.beauty-line-icon-theme;
         };
-        # the dark files are not copied by default, as not all themes have separate files
-        # see: https://github.com/nix-community/home-manager/blob/afcedcf2c8e424d0465e823cf833eb3adebe1db7/modules/misc/gtk.nix#L238
-        extraCss = ''
-          @import url("file://${theme.package}/share/themes/${theme.name}/gtk-4.0/gtk-dark.css");
-        '';
+        theme = {
+          name = "Juno-ocean";
+          package = pkgs.juno-theme;
+        };
+        gtk4 = {
+          extraConfig = {
+            gtk-application-prefer-dark-theme = true;
+          };
+          # the dark files are not copied by default, as not all themes have separate files
+          # see: https://github.com/nix-community/home-manager/blob/afcedcf2c8e424d0465e823cf833eb3adebe1db7/modules/misc/gtk.nix#L238
+          extraCss = ''
+            @import url("file://${cfg.theme.package}/share/themes/${cfg.theme.name}/gtk-4.0/gtk-dark.css");
+          '';
+          theme = cfg.theme;
+        };
       };
-    };
-  })
+    })
 ]
