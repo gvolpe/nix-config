5a13709be967173e1f47c254d705c45c139976fe
Gabriel Volpe <volpegabriel@gmail.com>
2026-05-28 16:43:20 +0200

    remove garnix ci (thank you for everything) 💔

--- flake.nix
+++ flake.nix
@@ -4,13 +4,11 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.nixos.org"
-      "https://cache.garnix.io"
       "https://gvolpe-nixos.cachix.org"
       "https://helium-nix.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "gvolpe-nixos.cachix.org-1:0MPlBIMwYmrNqoEaYTox15Ds2t1+3R+6Ycj0hZWMcL0="
       "helium-nix.cachix.org-1:a8YPjt9O4GPyX0u3gjg/aWpb14teU9aRiSG/MOaSFgw="
     ];
--- system/configuration.nix
+++ system/configuration.nix
@@ -197,12 +197,10 @@ in
       # Binary caches
       substituters = [
         "https://cache.nixos.org"
-        "https://cache.garnix.io"
         "https://gvolpe-nixos.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "gvolpe-nixos.cachix.org-1:0MPlBIMwYmrNqoEaYTox15Ds2t1+3R+6Ycj0hZWMcL0="
       ];
 
