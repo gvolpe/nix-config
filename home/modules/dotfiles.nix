186d4a268e050070cc241f83e1c27dbde0eb7ca6
Gabriel Volpe <volpegabriel@gmail.com>
2026-07-30 18:38:26 +0200

    dotfiles: fix symlinking logic

--- home/modules/dotfiles.nix
+++ home/modules/dotfiles.nix
@@ -3,6 +3,7 @@
 let
   inherit (config.lib.file) mkOutOfStoreSymlink;
   cfg = config.dotfiles;
+  sourcePath = ./..;
 in
 {
   options = {
@@ -14,7 +15,7 @@ in
       # Inputs
 
       `filepath`: the immutable configuration file path to link.
-      `rootpath`: the directory where the mutable file would be linked.
+      `rootpath`: the source directory containing the configuration file.
 
       # Type
 
@@ -28,36 +29,38 @@ in
       When `dotfiles.mutable = true`, it returns a symlink derivation:
 
       ```console
-      nix-repl> :b homeConfigurations.niri-hdmi.config.dotfiles.make ./LICENSE ./.
+      nix-repl> :b homeConfigurations.niri-hdmi.config.dotfiles.make ./home/programs/fastfetch/config.jsonc ./home/programs/fastfetch
 
       This derivation produced the following outputs:
-        out -> /nix/store/p62qfcfschb9g8fcgah4xvdk88b89mmx-hm_LICENSE
+        out -> /nix/store/p62qfcfschb9g8fcgah4xvdk88b89mmx-hm_config.jsonc
 
-      $ readlink -f /nix/store/p62qfcfschb9g8fcgah4xvdk88b89mmx-hm_LICENSE
-      /home/gvolpe/workspace/nix-config/home/LICENSE
+      $ readlink -f /nix/store/p62qfcfschb9g8fcgah4xvdk88b89mmx-hm_config.jsonc
+      /home/gvolpe/workspace/nix-config/home/programs/fastfetch/config.jsonc
       ```
 
       When `dotfiles.mutable = false`, it returns the direct Nix path literal (which HM copies to the store during activation):
 
       ```console
-      nix-repl> homeConfigurations.niri-hdmi.config.dotfiles.make ./LICENSE ./.
-      /home/gvolpe/workspace/nix-config/LICENSE
+      nix-repl> homeConfigurations.niri-hdmi.config.dotfiles.make ./home/programs/fastfetch/config.jsonc ./home/programs/fastfetch
+      /home/gvolpe/workspace/nix-config/home/programs/fastfetch/config.jsonc
 
-      nix-repl> cf = homeConfigurations.niri-hdmi.config.dotfiles.make ./LICENSE ./.
+      nix-repl> cf = homeConfigurations.niri-hdmi.config.dotfiles.make ./home/programs/fastfetch/config.jsonc ./home/programs/fastfetch
       nix-repl> "${cf}"
-      "/nix/store/n62mxdkhz8a16za5pm2k5n21blv28lkd-LICENSE"
+      "/nix/store/n62mxdkhz8a16za5pm2k5n21blv28lkd-config.jsonc"
 
-      readlink -f /nix/store/n62mxdkhz8a16za5pm2k5n21blv28lkd-LICENSE
-      /nix/store/n62mxdkhz8a16za5pm2k5n21blv28lkd-LICENSE
+      readlink -f /nix/store/n62mxdkhz8a16za5pm2k5n21blv28lkd-config.jsonc
+      /nix/store/n62mxdkhz8a16za5pm2k5n21blv28lkd-config.jsonc
       ```
 
       :::
       */
       make = lib.mkOption {
         default = filepath: rootpath:
-          let relpath = lib.path.removePrefix rootpath filepath;
+          let
+            rootRelpath = lib.path.removePrefix sourcePath rootpath;
+            fileRelpath = lib.path.removePrefix rootpath filepath;
           in if !cfg.mutable then filepath
-          else mkOutOfStoreSymlink "${cfg.path}/${relpath}";
+          else mkOutOfStoreSymlink "${cfg.path}/${rootRelpath}/${fileRelpath}";
         description = "Make an immutable or mutable config file";
       };
 
@@ -66,8 +69,8 @@ in
       path = lib.mkOption {
         apply = toString;
         default = "${config.home.homeDirectory}/workspace/nix-config/home";
-        description = "Location of the dotfiles working copy";
-        example = "${config.home.homeDirectory}/.dotfiles";
+        description = "Location of the mutable dotfiles home tree";
+        example = "${config.home.homeDirectory}/.dotfiles/home";
         type = lib.types.path;
       };
     };
