f5c636981e7a9714c36ee8227385a51f70685346
Gabriel Volpe <volpegabriel@gmail.com>
2026-08-07 08:52:56 +0200

    sway-audio-idle-inhibit: ignore source output as a patch

--- home/overlays/sway-audio-idle-inhibit/default.nix
+++ home/overlays/sway-audio-idle-inhibit/default.nix
@@ -1,27 +1,11 @@
 final: prev:
 
-let
-  newcode = ''
-    if (!appName) appName = pa_proplist_gets(i->proplist, PA_PROP_MEDIA_NAME);
-    if (appName) {
-  '';
-in
 {
   sway-audio-idle-inhibit = prev.sway-audio-idle-inhibit.overrideAttrs (old: {
     patches = (old.patches or [ ]) ++ [
       ./no-abort-on-logind-race.patch
-    ];
-
-    preConfigure = (old.preConfigure or "") + ''
-      # https://github.com/ErikReider/SwayAudioIdleInhibit/issues/36
-      substituteInPlace src/main.cpp \
-        --replace-fail 'strtok_r(argv[++i], " ", &saveptr);' 'strtok_r(argv[++i], ",", &saveptr);'
-      substituteInPlace src/main.cpp \
-        --replace-fail 'token = strtok_r(nullptr, " ", &saveptr);' 'token = strtok_r(nullptr, ",", &saveptr);'
-
       # https://github.com/ErikReider/SwayAudioIdleInhibit/pull/27
-      substituteInPlace src/pulse.cpp \
-        --replace-fail "if (appName) {" "${newcode}"
-    '';
+      ./ignore-source-output-improvements.patch
+    ];
   });
 }
