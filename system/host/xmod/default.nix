e93001865f27d29a495b1c6e68990d277f30e3c9
Gabriel Volpe <volpegabriel@gmail.com>
2026-09-12 21:40:17 +0200

    fix xmod config

--- system/host/xmod/default.nix
+++ system/host/xmod/default.nix
@@ -6,9 +6,7 @@ in
 {
   imports = [
     ./hardware-configuration.nix
-    inputs.agenix.homeManagerModules.default
     inputs.home-manager.nixosModules.home-manager
-    (import ../../../home/secrets)
   ];
 
   home-manager = {
@@ -16,12 +14,14 @@ in
     useGlobalPkgs = true;
 
     sharedModules = [
+      inputs.agenix.homeManagerModules.default
       inputs.dots.homeModules.default
       inputs.neovim-flake.homeModules.default
       inputs.nix-index.homeModules.default
       ({ nix.registry.nixpkgs.flake = inputs.nixpkgs; })
       (import ../../../home/dotfiles.nix { mutable = false; })
       { hidpi = false; }
+      (import ../../../home/secrets)
     ];
 
     users.gvolpe = import ./home.nix;
