6f3ecabc0b1a4cf55a9daed66765c75b01bd1761
Gabriel Volpe <volpegabriel@gmail.com>
2026-09-24 12:39:51 +0200

    fastfetch: remove xdg config file

--- home/programs/fastfetch/default.nix
+++ home/programs/fastfetch/default.nix
@@ -1,14 +1,11 @@
 { config, lib, pkgs, ... }:
 
-let
-  cfg = config.dotfiles.make ./config.jsonc;
-in
 {
   wrappers.fastfetch = lib.mkWrapper {
     basePackage = pkgs.fastfetch;
-    prependFlags = [ "--config" cfg ];
+    prependFlags = [
+      "--config"
+      (config.dotfiles.make ./config.jsonc)
+    ];
   };
-
-  # this is not required, but leaving it here for demo purposes
-  xdg.configFile."fastfetch/config.jsonc".source = cfg;
 }
