eb730cc37e4f08516c1ab782a683ca2925e5a88c
Gabriel Volpe <volpegabriel@gmail.com>
2026-09-24 12:16:56 +0200

    swaync: use wrapper instead of xdg config files

--- home/services/swaync/default.nix
+++ home/services/swaync/default.nix
@@ -1,22 +1,18 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
-let
-  cfg = config.services.swaync;
-  jsonConfig = builtins.fromJSON (builtins.readFile ./config.json);
-
-  style = config.dotfiles.make ./style.css;
-in
 {
   services.swaync = {
-    inherit style;
     enable = true;
-    settings = lib.mkIf (!config.dotfiles.mutable) jsonConfig;
+    package = config.wrappers.swaync;
   };
 
-  xdg.configFile = {
-    "swaync/config.json" = lib.mkIf config.dotfiles.mutable {
-      source = lib.mkForce (config.dotfiles.make ./config.json);
-    };
-    "swaync/configSchema.json".source = "${cfg.package}/etc/xdg/swaync/configSchema.json";
+  wrappers.swaync = lib.mkWrapper {
+    basePackage = pkgs.swaynotificationcenter;
+    prependFlags = [
+      "--config"
+      (config.dotfiles.make ./config.json)
+      "--style"
+      (config.dotfiles.make ./style.css)
+    ];
   };
 }
